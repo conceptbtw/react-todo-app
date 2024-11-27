d4634491002ab65af93ebc4268c869a2541613a3
Illia Movchko <illia.movchko.concept@gmail.com>
2024-11-27 07:47:59 +0000

    Update AddToDo component

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,7 +1,28 @@
-const AddToDo = ({ addTodo }) => {
+import { useState } from "react";
+
+const AddToDo = ({ addToDo }) => {
+  const [toDo, setToDo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (toDo.trim()) {
+      addToDo(toDo);
+      setToDo("");
+    }
+  };
+
   return (
-    <form className="w-full p-1 flex justify-center items-center gap-1 border rounded">
-      <input className="h-8 p-1 flex-1" type="text" placeholder="Add ToDo..." />
+    <form
+      className="w-full p-1 flex justify-center items-center gap-1 border rounded"
+      onSubmit={handleSubmit}
+    >
+      <input
+        className="h-8 p-1 flex-1"
+        type="text"
+        placeholder="Add ToDo..."
+        value={toDo}
+        onChange={(e) => setToDo(e.target.value)}
+      />
       <button className="size-8 border rounded" type="submit">
         <i className="fa-solid fa-plus"></i>
       </button>
